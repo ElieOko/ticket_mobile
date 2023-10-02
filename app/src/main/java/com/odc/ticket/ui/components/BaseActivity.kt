a4b115740c233d7a0e6660199b4ad1880e771d76
Elie Oko <elieoko100@gmail.com>
2023-10-02 12:55:17 +0100

    feat:add style, add permission, customisation base activity and fragment, add color

---
+++ app/src/main/java/com/odc/ticket/ui/components/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.odc.ticket.ui.components
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.odc.ticket.R
+
+abstract class BaseActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.MaterialTheme_NoActionBar)
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/odc/ticket/ui/components/main/MainActivity.kt
+++ app/src/main/java/com/odc/ticket/ui/components/main/MainActivity.kt
@@ -3,8 +3,9 @@ package com.odc.ticket.ui.components.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odc.ticket.R
+import com.odc.ticket.ui.components.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
