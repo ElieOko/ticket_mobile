08b2d2083015261cad88bc720c7698f02cf29b99
Elie Oko <elieoko100@gmail.com>
2023-10-04 16:06:46 +0100

    fix:first launch

--- app/src/main/java/com/odc/ticket/ui/components/login/LoginActivity.kt
+++ app/src/main/java/com/odc/ticket/ui/components/login/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.odc.ticket.ui.components.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.odc.ticket.R
 
 class LoginActivity : AppCompatActivity() {
+    private var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment?
+        if (navHostFragment != null) navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController!!.navigateUp()
     }
 }
--- app/src/main/java/com/odc/ticket/ui/components/main/MainActivity.kt
+++ app/src/main/java/com/odc/ticket/ui/components/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.odc.ticket.ui.components.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import com.odc.ticket.R
 import com.odc.ticket.ui.components.BaseActivity
+import com.odc.ticket.ui.components.login.LoginActivity
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        startActivity(Intent(this, LoginActivity::class.java))
+//        finish()
     }
 }
